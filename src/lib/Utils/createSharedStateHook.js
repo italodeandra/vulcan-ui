fa6adcbeaabedb91a67623e8f896d61bc9be1747
Ítalo Andrade <italodeandra@gmail.com>
2019-09-09 21:04:03 -0300

    Fix: Wait until layout rendering before triggering the first setState

--- src/lib/Utils/createSharedStateHook.js
+++ src/lib/Utils/createSharedStateHook.js
@@ -1,35 +1,45 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 function createSharedStateHook(initialState) {
-    let state = initialState
-    const listeners = []
-    const stateRef = {}
+  let state = initialState
+  const listeners = []
+  const stateRef = {}
 
-    function useSharedState() {
-        const [sharedState, setSharedState] = useState(state)
+  function useSharedState() {
+    const [sharedState, setSharedState] = useState(state)
+    const ready = useRef(false)
 
-        const setAllStates = (newState) => {
-            state = newState
-            stateRef.value = state
-            listeners.forEach(setState => setState(state))
-        }
+    useLayoutEffect(() => {
+      ready.current = true
+      setTimeout(() => {
+        listeners.forEach(setState => setState(state))
+      }, 100)
+    }, [])
 
-        useEffect(() => {
-            listeners.push(setSharedState)
-
-            return () => {
-                listeners.splice(listeners.indexOf(setSharedState), 1)
-            }
-        }, [])
-
-        return [
-            sharedState,
-            setAllStates,
-            stateRef
-        ]
+    const setAllStates = (newState) => {
+      if (ready.current) {
+        state = newState
+        stateRef.value = state
+        listeners.forEach(setState => setState(state))
+      }
     }
 
-    return useSharedState
+    useEffect(() => {
+      listeners.push(setSharedState)
+
+      return () => {
+        listeners.splice(listeners.indexOf(setSharedState), 1)
+      }
+    }, [])
+
+    return [
+      sharedState,
+      setAllStates,
+      stateRef
+    ]
+  }
+
+  return useSharedState
 }
 
 export default createSharedStateHook
